42921cf0b356ed404a1614be30e3ec11ca6275d9
Ruzaini Ibrahim <ruzainibrahim96@gmail.com>
2020-08-12 10:26:56 +0800

    middleware: change autoredirect route for admin

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -21,7 +21,7 @@ class RedirectIfAuthenticated
         switch ($guard) {
             case 'admin':
                 if (Auth::guard($guard)->check()) {
-                    return redirect()->route('user.index');
+                    return redirect()->route('admin.dashboard');
                 }
                 break;
             
